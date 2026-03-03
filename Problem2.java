d73994383ea46fac8a588c8317b749c1d783de73
Tanvir H Rubayet <tanvirrubayet20@gmail.com>
2026-03-04 00:22:03 +0600

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Problem2 {
+    public static void main(String[] arg)
+  {
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        System.out.print("Hello ");
+        System.out.println(name);
+
+    }
+}
