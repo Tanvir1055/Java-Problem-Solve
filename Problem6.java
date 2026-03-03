e699d036c9f429e55fd11a42f25fd22db938ef6d
Tanvir H Rubayet <tanvirrubayet20@gmail.com>
2026-03-04 00:59:33 +0600

    Create Problem6.java

---
+++ Problem6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Problem6 
+{
+    public static void main(String[] arg) 
+    {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
